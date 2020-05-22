308b7aed4f656e7599bc210ac444323b21d4decf
Oleksandr Menchynskyi <the.mngs@yahoo.com>
2020-05-22 12:36:17 +0300

    Add eslint and prettier to the project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,16 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { NextPage } from 'next';
 
-const Home: React.FC = () => {
+const Home: NextPage = () => {
   return (
     <div className="container">
       <Head>
         <title>Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        Blog
-      </main>
+      <main>Blog</main>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
